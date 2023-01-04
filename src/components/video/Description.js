5760b5c46a03e3ac90f4358d38806a16f4b00e56
muhaiminulIslamIkhlas <muhaiming2c@gmail.com>
2023-01-05 00:42:45 +0600

    complete delete

--- src/components/video/Description.js
+++ src/components/video/Description.js
@@ -1,15 +1,22 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import deleteImage from "../../assets/delete.svg";
 import editImage from "../../assets/edit.svg";
 import { useDeleteVideoMutation } from "../../features/api/apiSlice";
 
-export default function Description({video}) {
-    const {title, date, id, description } = video;
-    const [deleteVideo, {isSuccess, isLoading, isError}] = useDeleteVideoMutation();
-
+export default function Description({ video }) {
+    const { title, date, id, description } = video;
+    const [deleteVideo, { isSuccess, isLoading, isError }] = useDeleteVideoMutation();
     const handleDelete = () => {
-        deleteVideo(id);
+        if(id) deleteVideo({ id });
     }
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isSuccess) navigate("/");
+    }, [isSuccess, navigate]);
+
     return (
         <div>
             <h1 className="text-lg font-semibold tracking-tight text-slate-800">
@@ -24,11 +31,11 @@ export default function Description({video}) {
                     <div className="flex gap-1 cursor-pointer">
                         <div className="shrink-0">
                             <Link to={`/videos/edit/${id}`}>
-                            <img
-                                className="w-5 block"
-                                src={editImage}
-                                alt="Edit"
-                            />
+                                <img
+                                    className="w-5 block"
+                                    src={editImage}
+                                    alt="Edit"
+                                />
                             </Link>
                         </div>
                         <Link to={`/videos/edit/${id}`}>
